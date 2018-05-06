2cb3a55db4669d1bcffed19956f514d815897d5b
Emmet <emmetmceneaney@gmail.com>
2018-05-07 00:15:27 +0100

    friends and user models done

---
+++ TheSongDb/TheSongDb/Models/Friend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheSongDb.Models
+{
+    public class Friend
+    {
+        public int friendId { get; set; }
+        public String friend1 { get; set; }
+        public String friend2 { get; set; }
+    }
+}
