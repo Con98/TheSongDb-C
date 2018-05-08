b826f1d7e540c4526e6d098da303bb14ba3e85e2
Thomas-MainPc <thomascon44@gmail.com>
2018-05-08 02:08:44 +0100

    2 factor working.  Style a bit

--- TheSongDb/TheSongDb/App_Start/IdentityConfig.cs
+++ TheSongDb/TheSongDb/App_Start/IdentityConfig.cs
@@ -16,6 +16,15 @@ namespace TheSongDb
 {
     public class EmailService : IIdentityMessageService
     {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
         public static class Keys
         {
             public static string SMSAccountIdentification = "ACbc1aa964d0b315fe4bf1ec836e8fdb73";
@@ -39,15 +48,6 @@ namespace TheSongDb
             return Task.FromResult(0);
             // Twilio End
 
-        }
-    }
-
-    public class SmsService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your SMS service here to send a text message.
-            return Task.FromResult(0);
         }
     }
 
